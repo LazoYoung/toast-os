a78961803c8aee9e1aa86605fd781edce9f3d9aa
Chanyoung Park <starkergame@gmail.com>
2023-04-06 10:05:55 +0900

    #1 Hide implementation of Process

--- src/toast/Process.java
+++ src/toast/Process.java
@@ -1,26 +1,8 @@
 package toast;
 
-public class Process {
-    private static int nextId = 0;
-    private final int pid;
-    private final int arrival;
-    private final int workload;
-
-    public Process(int arrival, int workload) {
-        this.pid = nextId++;
-        this.arrival = arrival;
-        this.workload = workload;
-    }
-
-    public int getProcessId() {
-        return pid;
-    }
-
-    public int getArrivalTime() {
-        return arrival;
-    }
-
-    public int getBurstTime(boolean pCore) {
-        return pCore ? (int) Math.ceil(workload / 2.0) : workload;
-    }
+public interface Process {
+    int getId();
+    int getArrivalTime();
+    int getWorkload();
+    int getRemainingWorkload();
 }
