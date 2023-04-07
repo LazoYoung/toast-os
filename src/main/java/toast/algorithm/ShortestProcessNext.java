8894208896171bb52e796c0cc7495ad18c77671d
lazoyoung <idealproduction@naver.com>
2023-04-07 17:45:14 +0900

    #1 Implement SPN algorithm

--- src/main/java/toast/algorithm/ShortestProcessNext.java
+++ src/main/java/toast/algorithm/ShortestProcessNext.java
@@ -1,12 +1,35 @@
 package toast.algorithm;
 
+import toast.api.Process;
+import toast.api.Processor;
 import toast.api.Scheduler;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Logger;
+
 public class ShortestProcessNext implements Algorithm {
+    private final Logger logger = Logger.getLogger("SPN");
+
     @Override
     public void run(Scheduler scheduler) {
-        // todo method stub
-        int time = scheduler.getElapsedTime();
-        System.out.printf("[SPN] Running... (%ss)%n", time);
+        Optional<Processor> idleProcessor = scheduler.getIdleProcessor();
+        List<Process> readyQueue = scheduler.getReadyQueue();
+
+        if (idleProcessor.isPresent() && !readyQueue.isEmpty()) {
+            Processor processor = idleProcessor.get();
+            Process process = readyQueue.stream()
+                    .min(Comparator.comparingInt(Process::getWorkload))
+                    .orElseThrow();
+            String coreName = processor.getCore().getName();
+            int pid = process.getId();
+
+            scheduler.dispatch(processor, process);
+            process.addCompletionListener(() -> System.out.printf("[SPN] Process #%d completed%n", pid));
+            System.out.printf("[SPN] Dispatched process #%d to %s%n", pid, coreName);
+        }
+
+        System.out.printf("[SPN] Elapsed time: %ds%n", scheduler.getElapsedTime());
     }
 }
