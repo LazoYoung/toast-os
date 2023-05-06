7222eb8f06eb663203caff58e13aa6e14e559620
Invidam <hansupark0208@gmail.com>
2023-05-06 21:45:22 +0900

    feat: 알고리즘 선택 구현

--- src/main/java/toast/ui/controller/SettingController.java
+++ src/main/java/toast/ui/controller/SettingController.java
@@ -1,5 +1,18 @@
 package toast.ui.controller;
 
-public class SettingController {
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import toast.enums.AlgorithmName;
 
+public class SettingController implements Initializable {
+    @FXML
+    private ChoiceBox<AlgorithmName> algorithmNameChoiceBox;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        algorithmNameChoiceBox.getItems().addAll(AlgorithmName.values());
+    }
 }
