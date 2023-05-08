7ee37ae17a6c1025421ee1f862407b6eeaaab015
Invidam <hansupark0208@gmail.com>
2023-05-08 10:41:27 +0900

    refactor: 이전 입력 방식 주석 처리

--- src/main/java/toast/MainApp.java
+++ src/main/java/toast/MainApp.java
@@ -37,7 +37,7 @@ public class MainApp extends Application {
     public static void main(String[] args) {
         Application.launch(args);
 
-//        startScheduler();
+//        startSchedulerWithTerminal();
     }
 
     private static void startSchedulerWithTerminal() {
