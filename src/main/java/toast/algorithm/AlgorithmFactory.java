87a83e4a9944890b4b44abf4444897f6711f568a
lazoyoung <idealproduction@naver.com>
2023-05-09 10:08:37 +0900

    refactor: switch 문으로 코드 간소화

--- src/main/java/toast/algorithm/AlgorithmFactory.java
+++ src/main/java/toast/algorithm/AlgorithmFactory.java
@@ -11,25 +11,13 @@ public class AlgorithmFactory {
     }
 
     public Algorithm create(Integer timeQuantum, Double initPower, Double powerThreshold) {
-        if (this.name.equals(AlgorithmName.FCFS)) {
-            return new FirstComeFirstService();
-        }
-        if (this.name.equals(AlgorithmName.RR)) {
-            return new RoundRobin(timeQuantum);
-        }
-        if (this.name.equals(AlgorithmName.HRRN)) {
-            return new HighResponseRatioNext();
-        }
-        if (this.name.equals(AlgorithmName.SPN)) {
-            return new ShortestProcessNext();
-        }
-        if (this.name.equals(AlgorithmName.SRTN)) {
-            return new ShortestRemainingTimeNext();
-        }
-        if (this.name.equals(AlgorithmName.CUSTOM)) {
-            return new CustomSatellite(timeQuantum, initPower, powerThreshold);
-        }
-
-        throw new RuntimeException("알맞는 알고리즘이 없음");
+        return switch (this.name) {
+            case FCFS -> new FirstComeFirstService();
+            case RR -> new RoundRobin(timeQuantum);
+            case HRRN -> new HighResponseRatioNext();
+            case SPN -> new ShortestProcessNext();
+            case SRTN -> new ShortestRemainingTimeNext();
+            case CUSTOM -> new CustomSatellite(timeQuantum, initPower, powerThreshold);
+        };
     }
 }
